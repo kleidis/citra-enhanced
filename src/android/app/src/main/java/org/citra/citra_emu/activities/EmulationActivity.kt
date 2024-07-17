0b3d7899a160a5a91fd6eb98f91cfcbeead95d0e
Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
2024-07-17 19:02:59 +0200

    android: fixed runtime settings not being editable after closing game
    Ishan09811

--- src/android/app/src/main/java/org/citra/citra_emu/activities/EmulationActivity.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/activities/EmulationActivity.kt
@@ -56,6 +56,8 @@ class EmulationActivity : AppCompatActivity() {
     private lateinit var screenAdjustmentUtil: ScreenAdjustmentUtil
     private lateinit var hotkeyUtility: HotkeyUtility
 
+    private var isEmulationRunning: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         ThemeUtil.setTheme(this)
 
@@ -87,6 +89,9 @@ class EmulationActivity : AppCompatActivity() {
         )
 
         EmulationLifecycleUtil.addShutdownHook(hook = { this.finish() })
+
+        isEmulationRunning = true
+        instance = this
     }
 
     // On some devices, the system bars will not disappear on first boot or after some
@@ -107,9 +112,21 @@ class EmulationActivity : AppCompatActivity() {
         NativeLibrary.reloadCameraDevices()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isEmulationRunning", isEmulationRunning)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isEmulationRunning = savedInstanceState.getBoolean("isEmulationRunning", false)
+    }
+
     override fun onDestroy() {
         NativeLibrary.enableAdrenoTurboMode(false)
         EmulationLifecycleUtil.clear()
+        isEmulationRunning = false
+        instance = null
         super.onDestroy()
     }
 
@@ -449,4 +466,12 @@ class EmulationActivity : AppCompatActivity() {
 
             OnFilePickerResult(result.toString())
         }
+
+    companion object {
+        private var instance: EmulationActivity? = null
+
+        fun isRunning(): Boolean {
+            return instance?.isEmulationRunning ?: false
+        }
+    }
 }
